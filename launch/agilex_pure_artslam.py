a86ce51e90200404ab2fdf12aef1aeb8ee397a25
Hi-Zed <gianluca.bardaro@gmail.com>
2023-07-26 09:57:10 +0200

    relative path for config file

--- launch/agilex_pure_artslam.py
+++ launch/agilex_pure_artslam.py
@@ -1,10 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
     ld = LaunchDescription()
+
+    config_file = os.path.join(
+      get_package_share_directory('artslam_wrapper'),
+      'config',
+      'KITTI.json'
+      )
 
     artslam_wrapper_node = Node(
         package='artslam_wrapper',
@@ -12,8 +19,8 @@ def generate_launch_description():
         name='wrapper_controller',
         output='screen',
         parameters=[{
-            'configuration_file': "/home/gianluca/development/humble/SLAM/artslam_ws/src/ARTSLAM_wrapper/config/KITTI.json",
-            'results_path': "/home/gianluca/development/humble/SLAM/artslam_ws/src/ARTSLAM_wrapper/results",
+            'configuration_file': config_file,
+            'results_path': '/LOTS/results/',
             # 'imu_topic': '/ouster/imu'
         }],
         # prefix=['xterm -e gdb -q -ex run --args']
